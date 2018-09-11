f4abe63564bacc04b1f3897edb51a4c26fdad45b
rahul96rajan <rahulrajan@qainfotech.com>
2018-09-11 23:43:21 +0530

    Added GoogleSheetReadUtil.

---
+++ src/main/java/com/java/googleSheet/ConfigUtil.java
@@ -0,0 +1,27 @@
+package com.java.googleSheet;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigUtil {
+	public static Properties prop;
+	public static void initialiseProperties() {	
+		try {
+			prop = new Properties();
+			FileInputStream in = new FileInputStream(System.getProperty("user.dir")
+					+ "/config.properties");
+			prop.load(in);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String getProperty(String value) {
+		initialiseProperties();
+		return prop.getProperty(value);
+	}
+}
---
+++ src/main/java/com/java/googleSheet/GoogleSheetsUtil.java
@@ -0,0 +1,66 @@
+package com.java.googleSheet;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+public class GoogleSheetsUtil {
+	private static String APPLICATION_NAME = "Google Sheets API Java Util To Get_Data";
+	private static JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+	private static String TOKENS_DIRECTORY_PATH = "tokens";
+	private static String range = "Test_Sheet!A1:G"; // Yaml
+//	private static String range = ConfigUtil.getProperty("range");
+	private static List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+	private static String CREDENTIALS_FILE_PATH = "/credentials.json";
+	
+
+	public static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+		InputStream in = SheetsQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
+				clientSecrets, SCOPES)
+						.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+						.setAccessType("offline").build();
+		return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+	}
+
+	public static List<List<Object>> pritnAllValues() throws GeneralSecurityException, IOException {
+		NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+		String spreadsheetId = "1RuCARQct2c5GZsAV8erEoHpYVywCqls7DKFy8Bx1LEI"; // Yaml
+//		String spreadsheetId = ConfigUtil.getProperty("spreadsheetId");
+		Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+				.setApplicationName(APPLICATION_NAME).build();
+		ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
+		List<List<Object>> values = response.getValues();
+		if (values == null || values.isEmpty()) {
+			System.out.println("No data found.");
+			return null;
+		} else {
+			return values;
+		}
+	}
+
+
+	
+	
+}
